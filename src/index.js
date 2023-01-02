d0ffe9fa053001db409c0b037ce6b6d266de3f59
AKWUDE RAJA <akwuderaja@gmail.com>
2023-01-02 14:53:58 +0100

    Fixed showing on both screens

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, globalShortcut , screen, dialog} = require('electron');
 const path = require('path');
 const {ipcMain} = require('electron')
 
-// ipcMain.on('resize-window', (event, width, height) => {
-//     let browserWindow = BrowserWindow.fromWebContents(event.sender)
-//     browserWindow.setSize(width,height)
-// })
 app.on('ready', () => {
     // Register a shortcut listener for Ctrl + Shift + I
-    globalShortcut.register('Control+Shift+I', () => {
-        // When the user presses Ctrl + Shift + I, this function will get called
-        // You can modify this function to do other things, but if you just want
-        // to disable the shortcut, you can just return false
-        return false;
-    });
-    globalShortcut.register('Esc', () => {
-        app.quit()
-    });
-    globalShortcut.register('Space', () => {
-        app.quit()
-    });
-    
-    app.addListener("keydown", ()=>{app.quit()}, true);
-    app.addListener("keyup", ()=>{app.quit()}, true);
-    app.addListener("mousemove", ()=>{app.quit()}, true);
+    // globalShortcut.register('Control+Shift+I', () => {
+    //     // When the user presses Ctrl + Shift + I, this function will get called
+    //     // You can modify this function to do other things, but if you just want
+    //     // to disable the shortcut, you can just return false
+    //     return false;
+    // });
     
   var date0 = new Date(2023, 0, 1); // 1 Jan 2015
   var date1 = new Date(); 
 
   var numberOfDays = Math.ceil((date1 - date0) / 8.64e7);
   if(numberOfDays>= 7){
-    app.quit();
+    app.quit(0);
     return;
   }
 });
@@ -38,41 +24,50 @@ app.on('ready', () => {
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    frame: false,
-    show: true,
-    // win.maximize();
-    // win.show();
-    fullscreen: true,
-    acceptFirstMouse: true,
-    alwaysOnTop: true,
-    minimizable: false,
-    icon: path.join(__dirname, 'images/logo.png'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      devTools: false,
-      nodeIntegration: false, // is default value after Electron v5
-      contextIsolation: true, // protect against prototype pollution
-      enableRemoteModule: false, // turn off remote
-    },
-  });
-  mainWindow.hookWindowMessage(0x200, () => {
-    app.quit();
-  });
-  mainWindow.hookWindowMessage(0x202, () => {
-    app.quit();
-  });
-
-
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  // Create the browser window.  
+  const displays = screen.getAllDisplays();
+  const externalDisplays = displays;
+  // .find((display) => {
+  //   return display.bounds.x !== 0 || display.bounds.y !== 0
+  // })
+  
+  // console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
+  for (let index = 0; index < externalDisplays.length; index++) {
+    // Create a window that fills the screen's available work area.
+    const s = externalDisplays[index];
+    // dialog.showMessageBox({ properties: ['message', s.bounds.x] });
+    // const primaryDisplay = screen.getPrimaryDisplay()
+    const { width, height, x, y } = s.bounds;
+    const newWindow = new BrowserWindow({
+      width: width,
+      height: height,
+      x: x, y: y,
+      frame: false,
+      show: true,
+      fullscreen: true,
+      acceptFirstMouse: true,
+      alwaysOnTop: true,
+      skipTaskbar: true,
+      // minimizable: false,
+      icon: path.join(__dirname, 'images/logo.png'),
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        devTools: false,
+        nodeIntegration: false, // is default value after Electron v5
+        contextIsolation: true, // protect against prototype pollution
+        enableRemoteModule: false, // turn off remote
+      },
+    });
+    // and load the index.html of the app.
+    newWindow.loadFile(path.join(__dirname, 'index.html'));
+    newWindow.on("closed", () => {
+      // newWindow = null;
+      app.quit(0);
+    });
+    if(index==0){
+    }
+  }
 };
 
 // This method will be called when Electron has finished
--- src/preload.js
+++ src/preload.js
@@ -1,15 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-// const {ipcRenderer} = require('electron');
 
-// Right after the line where you changed the document.location
-// ipcRenderer.send('resize-me-please')
-// ipcRenderer.send('resize-window', 1280, 720)
+window.addEventListener('mousemove', ()=>{
+  window.close();
+});
+window.addEventListener('mouseclick', ()=>{
+  window.close();
+});
 
-
-window.addEventListender('mousemove', ()=>{
-    window.close();
-  });
-  
-  window.addEventListener('keyup', app.quit(), true)
+window.addEventListener('keydown', ()=>{
+  window.close();
+});
 
